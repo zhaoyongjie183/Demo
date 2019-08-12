524476f221c20cb2b3eeeae47152551c814d8f45
zyj <893722059@qq.com>
2019-08-12 17:51:24 +0800

    update

--- RedisProject/Program.cs
+++ RedisProject/Program.cs
@@ -9,9 +9,12 @@ namespace RedisProject
 
             RedisHelper redisHelper = new RedisHelper();
             var c=redisHelper.GetConnectionMultiplexer();
-            
-           // redisHelper.StringSet("cache:a", "BBBB",TimeSpan.FromSeconds(20));
-            redisHelper.HashSet("cache:a", "BBBB", TimeSpan.FromSeconds(20));
+
+            // redisHelper.StringSet("cache:a", "BBBB",TimeSpan.FromSeconds(20));
+            //redisHelper.HashSet("cache:a", "BBBB", TimeSpan.FromSeconds(20));
+
+            var flag=redisHelper.LockTake("cache:b", "a",TimeSpan.FromMinutes(60));
+           // var aa = redisHelper.LockRelease("cache:b", "a");
             Console.WriteLine("设置缓存成功");
             Console.ReadLine();
         }
--- RedisProject/RedisHelper.cs
+++ RedisProject/RedisHelper.cs
@@ -75,6 +75,27 @@ namespace RedisProject
             }
         }
 
+
+        public  bool LockTake(string key, string data, TimeSpan validFor)
+        {
+            // key：用key来当锁，因为key是唯一的。
+            // value：很多童鞋可能不明白，有key作为锁不就够了吗，为什么还要用到value?原因就是我们在上面讲到可靠性时，
+            // 分布式锁要满足第四个条件解铃还须系铃人，通过给value赋值为Guid.NewGuid().ToString()，我们就知道这把锁是哪个请求加的了，在解锁的时候就可以有依据。
+            var redisCon = GetConnectionMultiplexer();
+            var db = GetRedisDB(redisCon);
+            return db.LockTake(key, data, validFor);
+        }
+
+        /// <summary>
+        /// 解锁
+        /// </summary>
+        public  bool LockRelease(string key, string data)
+        {
+            var redisCon = GetConnectionMultiplexer();
+            var db = GetRedisDB(redisCon);
+            return db.LockRelease(key, data);
+        }
+
         public void HashSet(string key, object value, TimeSpan? validFor)
         {
             try
